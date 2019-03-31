9fa02c66eee91c8a0510fca61650a29e16dde071
Veselin Nikolov <vnikolov88@gmail.com>
2019-03-31 20:39:27 +0300

    Changes in crawler

--- DDAppNative.AppCreator/Primers/HttpAppCrawler.cs
+++ DDAppNative.AppCreator/Primers/HttpAppCrawler.cs
@@ -22,31 +22,38 @@ namespace DDAppNative.AppCreator.Primers
                 Console.WriteLine(cacheList[i]);
                 // Don't load more then one layer of external resources
                 if (cacheList[i].StartsWith("http")) continue;
+                try
+                {
+                    var uri = new Uri(_appHostBase, $"{cacheList[i]}").AbsoluteUri;
+                    var doc = await new HtmlWeb().LoadFromWebAsync(uri);
+                    var resources = doc.DocumentNode.Descendants("link")
+                                                      .Select(a => a.GetAttributeValue("href", null))
+                                                      .Where(u => !string.IsNullOrEmpty(u)).ToList();
+                    resources.AddRange(doc.DocumentNode.Descendants("a")
+                                                      .Select(a => a.GetAttributeValue("href", null))
+                                                      .Where(u => !string.IsNullOrEmpty(u)).ToList()
+                                                      );
+                    resources.AddRange(doc.DocumentNode.Descendants("script")
+                                                      .Select(a => a.GetAttributeValue("src", null))
+                                                      .Where(u => !string.IsNullOrEmpty(u)).ToList()
+                                                      );
+                    resources.AddRange(doc.DocumentNode.Descendants("img")
+                                                      .Select(a => a.GetAttributeValue("src", null))
+                                                      .Where(u => !string.IsNullOrEmpty(u)).ToList()
+                                                      );
 
-                var uri = new Uri(_appHostBase, $"{cacheList[i]}").AbsoluteUri;
-                var doc = await new HtmlWeb().LoadFromWebAsync(uri);
-                var resources = doc.DocumentNode.Descendants("link")
-                                                  .Select(a => a.GetAttributeValue("href", null))
-                                                  .Where(u => !string.IsNullOrEmpty(u)).ToList();
-                resources.AddRange(doc.DocumentNode.Descendants("a")
-                                                  .Select(a => a.GetAttributeValue("href", null))
-                                                  .Where(u => !string.IsNullOrEmpty(u)).ToList()
-                                                  );
-                resources.AddRange(doc.DocumentNode.Descendants("script")
-                                                  .Select(a => a.GetAttributeValue("src", null))
-                                                  .Where(u => !string.IsNullOrEmpty(u)).ToList()
-                                                  );
-                resources.AddRange(doc.DocumentNode.Descendants("img")
-                                                  .Select(a => a.GetAttributeValue("src", null))
-                                                  .Where(u => !string.IsNullOrEmpty(u)).ToList()
-                                                  );
+                    var tempCahce = cacheList.ToList();
+                    // Full URL
+                    tempCahce.AddRange(resources.Where(x => !x.Contains("ondevice")).Select(x => x.Replace("./", $"/{appCode}/")).Distinct());
+                    // Partial Copy
+                    tempCahce.AddRange(resources.Where(x => !x.Contains("ondevice") && !x.StartsWith('.') && !x.StartsWith('/') && !x.StartsWith("http")).Distinct().Select(x => $"/partial/{appCode}/{x}").ToList());
+                    cacheList = tempCahce.Distinct().ToArray();
 
-                var tempCahce = cacheList.ToList();
-                // Full URL
-                tempCahce.AddRange(resources.Where(x => !x.Contains("ondevice")).Select(x => x.Replace("./", $"/{appCode}/")).Distinct());
-                // Partial Copy
-                tempCahce.AddRange(resources.Where(x => !x.Contains("ondevice") && !x.StartsWith('.') && !x.StartsWith('/') && !x.StartsWith("http")).Distinct().Select(x => $"/partial/{appCode}/{x}").ToList());
-                cacheList = tempCahce.Distinct().ToArray();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Unable to load {cacheList[i]} skipping...");
+                }
             }
 
             return cacheList;
